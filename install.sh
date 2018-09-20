1e1a5f5fc1b26588e25bd9ecf922c16a8ea2c6f5
KoHey94 <sugawara622@gmail.com>
2018-09-20 15:22:12 +0900

    もろもろ修正

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#Please clone dotfiles to ~/.vim.
+#Please clone dotfiles to ~/.vim/dotfiles.
 
 #Use vim-plug as plugin manager.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cp ~/.vim/dotfiles/.vimrc ~/
-git clone https://github.com/w0ng/vim-hybrid ~/.vim/
+git clone https://github.com/w0ng/vim-hybrid ~/.vim/vim-hybrid
 mkdir ~/.vim/colors
 cp ~/.vim/vim-hybrid/colors/hybrid.vim ~/.vim/colors/ 
